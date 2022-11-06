fdcf4c87cfd18729fa136860d85c943932be7fa5
0mni-gear515 <adamyi515@gmail.com>
2022-11-06 13:53:16 -0600

    Setup initial file structure. Create components, context directory. Finish creating the header component.

---
+++ src/components/shared/header/header.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.style.css';
+
+const Header = () => {
+    return(
+        <header className='header'>
+            <h1 className='header-brand'>Feedback UI</h1>
+        </header>
+    )
+}
+
+export default Header;
